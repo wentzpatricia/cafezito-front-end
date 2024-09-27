c7eb415f9ef0625e0d02fa95a7d786a3c8a9f4c6
wentzpatricia <patriciawentzm@gmail.com>
2024-09-27 20:33:48 -0300

    feat: criando componente reutilizavel botao voltar

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
@@ -21,7 +22,9 @@ import { RegisterComponent } from './register/register.component';
     InputTextModule,
     FormsModule,
     NgOptimizedImage,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
+
   ]
 })
 export class AuthModule {}
