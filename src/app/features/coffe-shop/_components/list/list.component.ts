9ac41a51b4331592c5404133917ea42f94d82e70
wentzpatricia <patriciawentzm@gmail.com>
2024-11-19 17:40:35 -0300

    feat: adicionando variaveis ao list

--- src/app/features/coffe-shop/_components/list/list.component.ts
+++ src/app/features/coffe-shop/_components/list/list.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ListCoffeeShopService } from '../../_services/list-coffee-shop.service';
+
 import { CoffeeShop } from '../../_models/list-coffee.interface';
 
 @Component({ selector: 'app-list', templateUrl: './list.component.html', styleUrl: './list.component.scss' })
 export class ListComponent implements OnInit {
+
+  listCoffeeShop: CoffeeShop[] = [];
+  loading: boolean = false;
 
   constructor(
     private listCoffeeShopService : ListCoffeeShopService
   ){}
   
   ngOnInit(): void {
+    this.getAllCoffeeShop();
+  }
+
+  getAllCoffeeShop() {
+    this.loading = true;
+
     this.listCoffeeShopService.getAllCoffeeShop().subscribe({
       next: (res: CoffeeShop[]) => {
         console.log(res)
-      }, error: (err) => {
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
         console.error(err)
       }
     })
