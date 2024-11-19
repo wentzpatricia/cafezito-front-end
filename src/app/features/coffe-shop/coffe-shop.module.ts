ba29592008212435718242d41c10d2a6d4c7afdd
wentzpatricia <patriciawentzm@gmail.com>
2024-11-19 15:33:52 -0300

    feat: adicionando estrutura para listagem das cafeterias

--- src/app/features/coffe-shop/coffe-shop.module.ts
+++ src/app/features/coffe-shop/coffe-shop.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CoffeShopRoutingModule } from './coffe-shop-routing.module';
 import { CoffeShopComponent } from './coffe-shop.component';
-
+import { FilterComponent } from './_components/filter/filter.component';
+import { ListComponent } from './_components/list/list.component';
+import { DetailComponent } from './_components/detail/detail.component';
 
 @NgModule({
-  declarations: [CoffeShopComponent],
-  imports: [
-    CommonModule,
-    CoffeShopRoutingModule
-  ]
+  declarations: [
+    CoffeShopComponent,
+    DetailComponent,
+    FilterComponent,
+    ListComponent,
+  ],
+  imports: [CommonModule, CoffeShopRoutingModule],
 })
-export class CoffeShopModule { }
+export class CoffeShopModule {}
