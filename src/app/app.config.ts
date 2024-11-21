9dc2dcb5503bf122b5bdb92388d755ec02530ce5
wentzpatricia <patriciawentzm@gmail.com>
2024-11-21 15:07:57 -0300

    feat: adicionando listagem das avaliações

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DatePipe } from '@angular/common';
+import { DatePipe, registerLocaleData } from '@angular/common';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { CoreModule } from './core/core.module';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +18,7 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(BrowserModule),
     importProvidersFrom(BrowserAnimationsModule),
     importProvidersFrom(CoreModule),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     DatePipe,
     provideHttpClient(),
   ],
--- src/app/features/coffe-shop/_components/list-rating/list-rating.component.ts
+++ src/app/features/coffe-shop/_components/list-rating/list-rating.component.ts
@@ -3,5 +3,12 @@ import { Rating } from '../../_models/list-coffee.interface';
 
 @Component({ selector: 'app-list-rating', templateUrl: './list-rating.component.html', styleUrl: './list-rating.component.scss' })
 export class ListRatingComponent {
+  
   @Input() rating!: Rating[];
+  @Input() average: number = 0;
+
+  createRange(count: number): number[] {
+    return Array.from({ length: count }, (_, i) => i);
+  }
+
 }
