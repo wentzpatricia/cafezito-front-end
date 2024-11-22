903d7827fbf6504468317e097fc7f53198510c29
wentzpatricia <patriciawentzm@gmail.com>
2024-11-21 23:32:10 -0300

    feat: adicionando opção de adiciionar avaliacao

--- src/app/features/coffe-shop/_components/list-rating/list-rating.component.ts
+++ src/app/features/coffe-shop/_components/list-rating/list-rating.component.ts
@@ -7,8 +7,26 @@ export class ListRatingComponent {
   @Input() rating!: Rating[];
   @Input() average: number = 0;
 
+  isDialogVisible = false;
+
   createRange(count: number): number[] {
     return Array.from({ length: count }, (_, i) => i);
+  }
+
+  handleFormSubmit(event: Rating ) {
+
+    event ? (this.rating.unshift(event), this.updateAverage(event)) : '';
+
+    this.isDialogVisible = false;
+  }
+
+  showDialog() {
+    this.isDialogVisible = true;
+  }
+
+  updateAverage(newRating: Rating) {
+    const totalItems = this.rating.length;
+    this.average = ((this.average * (totalItems - 1)) + newRating.stars) / totalItems;
   }
 
 }
--- src/app/features/coffe-shop/coffe-shop.module.ts
+++ src/app/features/coffe-shop/coffe-shop.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CoffeShopRoutingModule } from './coffe-shop-routing.module';
 import { CoffeShopComponent } from './coffe-shop.component';
 import { DetailComponent } from './_components/detail/detail.component';
+import { DialogModule } from 'primeng/dialog';
 import { FilterComponent } from './_components/filter/filter.component';
 import { FormRatingComponent } from './_components/form-rating/form-rating.component';
 import { ListComponent } from './_components/list/list.component';
 import { ListRatingComponent } from './_components/list-rating/list-rating.component';
 
 import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { RatingModule } from 'primeng/rating';
 import { SharedModule } from '../../shared/shared.module';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -21,6 +30,19 @@ import { SharedModule } from '../../shared/shared.module';
     ListComponent,
     ListRatingComponent
   ],
-  imports: [ButtonModule, CommonModule, CoffeShopRoutingModule, SharedModule],
+  imports: [
+    ButtonModule, 
+    CommonModule, 
+    CoffeShopRoutingModule, 
+    DialogModule,
+    InputTextareaModule,
+    FloatLabelModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RatingModule,
+    SharedModule,
+    ToastModule
+  ],
+  providers:[MessageService]
 })
 export class CoffeShopModule {}
