8a61f75c1f6ca75228170a6bb496721299ddfaeb
Patrícia <85044012+wentzpatricia@users.noreply.github.com>
2024-11-24 20:20:22 -0300

    feat: adicionando filtro (#9)

--- src/app/features/coffe-shop/_components/filter/filter.component.ts
+++ src/app/features/coffe-shop/_components/filter/filter.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProductTagEnum } from '../../_models/coffee-shop.enum';
 
-@Component({
-  selector: 'app-filter',
-  templateUrl: './filter.component.html',
-  styleUrl: './filter.component.scss',
-})
-export class FilterComponent {}
+@Component({ selector: 'app-filter', templateUrl: './filter.component.html', styleUrl: './filter.component.scss' })
+export class FilterComponent implements OnInit {
+  @Output() filtersChanged = new EventEmitter<ProductTagEnum[]>();
+
+  form: FormGroup;
+  productTags = Object.entries(ProductTagEnum);
+
+  constructor(private fb: FormBuilder) {
+    const controls = this.productTags.reduce<{ [key in ProductTagEnum]?: boolean[] }>((acc, [key]) => {
+      acc[key as ProductTagEnum] = [false];
+      return acc;
+    }, {});
+  
+    this.form = this.fb.group(controls);
+  }
+  
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe(filters => {
+      const activeFilters = Object.keys(filters)
+        .filter(key => filters[key])
+        .map(key => key as ProductTagEnum);
+
+      this.filtersChanged.emit(activeFilters);
+    });
+  }
+  
+}
--- src/app/features/coffe-shop/_services/list-coffee-shop.service.ts
+++ src/app/features/coffe-shop/_services/list-coffee-shop.service.ts
@@ -3,14 +3,18 @@ import { Observable } from 'rxjs';
 
 import { RequestsService } from '../../../core/services/requests.service';
 import { CoffeeShop, CoffeeShopDetail } from '../_models/list-coffee.interface';
+import { ProductTagEnum } from '../_models/coffee-shop.enum';
 
 @Injectable({ providedIn: 'root' })
 export class ListCoffeeShopService {
     
     constructor(private requestsService: RequestsService) {}
     
-    getAllCoffeeShop(): Observable<CoffeeShop[]>{
-        const url = `coffee-shop`;
+    getAllCoffeeShop(tags?: ProductTagEnum[]): Observable<CoffeeShop[]> {
+        console.log(tags)
+        const tagsParam = tags && tags.length > 0 ? tags.map(tag => `tags=${tag}`).join('&') : '';
+        const url = tagsParam ? `coffee-shop?${tagsParam}` : `coffee-shop`;
+    
         return this.requestsService.executeGet({ url });
     }
 
