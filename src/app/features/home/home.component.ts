39d158ce6d22d3c3b913f9daad92982787c56be1
Mariana Dreyer <mariana.odreyer@gmail.com>
2024-11-27 10:30:23 -0300

    ajuste imagens home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -7,14 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent { 
-  graosHome = '/src/assets/images/graos.png.';
-  cafezitoHomeUm = '/src/assets/images/cafezito-home-1.png';
-  cafezitoHomeDois = '/src/assets/images/cafezito-home-2.png';
-  cafezitoHomeTres = '/src/assets/imaghahahhaes/cafezito-home-3.png';
-  cafezitoHomeQuatro = '/src/assets/images/cafezito-home-4.png';
-  cafezitoHomeCinco = '/src/assets/images/cafezito-home-5.png';
-  placaCoffee = '/src/assets/images/placa-coffee.png';
-  homemCafeteria = '/src/assets/images/homem-cafeteria.png';
-  aguaGas = '/src/assets/images/agua-gas-cafe.png';
+  
+  graosHome = '../../../assets/images/graos.png';
+  cafezitoHomeUm = '../../../assets/images/cafezito-home-1.png';
+  cafezitoHomeDois = '../../../assets/images/cafezito-home-2.png';
+  cafezitoHomeTres = '../../../assets/images/cafezito-home-3.png';
+  cafezitoHomeQuatro = '../../../assets/images/cafezito-home-4.png';
+  cafezitoHomeCinco = '../../../assets/images/cafezito-home-5.png';
+  placaCoffee = '../../../assets/images/placa-coffee.png';
+  homemCafeteria = '../../../assets/images/homem-cafeteria.png';
+  aguaGas = '../../../assets/images/agua-gas-cafe.png';
 
 }
