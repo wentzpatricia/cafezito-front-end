289f046631f34932277a370a6934fa5cd2b63a86
Mariana Dreyer <mariana.odreyer@gmail.com>
2024-11-28 09:35:04 -0300

    Estilização da home

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderHomeComponent } from './header-home/header-home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ButtonModule } from 'primeng/button';
@@ -9,7 +8,7 @@ import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule,HeaderHomeComponent,ButtonModule],
+  imports: [CommonModule, HomeRoutingModule,ButtonModule],
 
 })
 export class HomeModule {
