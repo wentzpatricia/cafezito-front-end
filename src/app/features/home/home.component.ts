4c8dfba707b79a1b67d70fe1ffc67733e1d4c3bb
Mariana Dreyer <mariana.odreyer@gmail.com>
2024-12-01 19:51:32 -0300

    Desenvolvimento home finalizada

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -19,6 +20,15 @@ export class HomeComponent {
   aguaGas = '../../../assets/images/agua-gas-cafe.png';
   logo = '../../../assets/icones/logo-dark.png'
 
-  
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/auth/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/auth/register']);
+  }
+
 
 }
