35325f1c58bf911f4fa3f9134f1a98f8da6aa5f5
wentzpatricia <patriciawentzm@gmail.com>
2024-12-01 21:46:29 -0300

    feat: listando vouchers

--- src/app/features/vouchers/_components/list-vouchers/list-vouchers.component.ts
+++ src/app/features/vouchers/_components/list-vouchers/list-vouchers.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { VouchersService } from '../../_services/vouchers.service';
+
+import { ListVouchers } from '../../_models/vouchers.interface';
 
 @Component({
   selector: 'app-list-vouchers',
   templateUrl: './list-vouchers.component.html',
   styleUrl: './list-vouchers.component.scss'
 })
-export class ListVouchersComponent {
+export class ListVouchersComponent implements OnInit {
 
+  listVouchers: ListVouchers[] = [];
+  loading: boolean = false;
+
+  constructor(private voucherService: VouchersService){}
+
+  ngOnInit(): void {
+    this.listAllVouchers();
+  }
+
+  listAllVouchers() {
+    this.loading = true;
+    this.voucherService.getAllVouchers().subscribe({
+      next: (res) => { 
+        this.listVouchers = res;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      }
+    })
+  }
 }
--- src/app/features/vouchers/vouchers.module.ts
+++ src/app/features/vouchers/vouchers.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ButtonModule } from 'primeng/button';
 import { TabViewModule } from 'primeng/tabview';
 
+import { SharedModule } from '../../shared/shared.module';
 import { VouchersRoutingModule } from './vouchers-routing.module';
+
 import { ListVouchersComponent } from './_components/list-vouchers/list-vouchers.component';
 import { UserVouchersComponent } from './_components/user-vouchers/user-vouchers.component';
 import { VouchersComponent } from './vouchers.component';
 
-
 @NgModule({
   declarations: [ListVouchersComponent, UserVouchersComponent, VouchersComponent],
   imports: [
+    ButtonModule,
     CommonModule,
+    SharedModule,
     TabViewModule,
     VouchersRoutingModule
   ]
