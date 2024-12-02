c4fb3e20b59a9930a1fbcb49b39f1095987d99d9
wentzpatricia <patriciawentzm@gmail.com>
2024-12-02 13:57:00 -0300

    feat: adicionando listagem user voucher

--- src/app/features/vouchers/_components/user-vouchers/user-vouchers.component.ts
+++ src/app/features/vouchers/_components/user-vouchers/user-vouchers.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UserVouchersService } from '../../_services/user-vouchers.service';
 
 @Component({
   selector: 'app-user-vouchers',
   templateUrl: './user-vouchers.component.html',
   styleUrl: './user-vouchers.component.scss'
 })
-export class UserVouchersComponent {
+export class UserVouchersComponent implements OnInit {
+
+  loading: boolean = false;
+  userVoucher: any [] = [];
+
+  constructor(private userVouchersService: UserVouchersService) {}
+
+  ngOnInit(): void {
+    this.listUserVoucher();
+  }
+
+  listUserVoucher() {
+    this.loading = true;
+    this.userVouchersService.getUserVouchers().subscribe({
+      next: (res) => {
+        this.userVoucher = res;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+        console.error(err)
+      }
+    })
+  }
 
 }
